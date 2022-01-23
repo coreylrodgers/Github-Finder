635e535fb38c07835b02f83e85d0f45750f76ccb
Corey <corey.rodgers@droneshield.com>
2022-01-23 11:01:54 +1100

    add spinner and github api req

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import spinner from './assets/spinner.gif'
+
+function Spinner() {
+  return <div className='w-100 mt-200'>
+      <img className='text-center mx-auto' width={180} src={spinner} alt="Loading..." />
+  </div>;
+}
+
+export default Spinner;
